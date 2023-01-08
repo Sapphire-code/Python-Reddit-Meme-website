6eda69efbe72cb9d9db4dc35d661b26cd489e288
SapphireKR <85494077+Sapphire-code@users.noreply.github.com>
2023-01-08 12:07:19 +0800

    fixed outdated url and etc.
    Added comments in code to tell fellow programmers on instructions to on how to disable NSFW memes.

--- main.py
+++ main.py
@@ -16,9 +16,12 @@ import json
 app = Flask(__name__)
 
 def get_meme():
-    # sr = "/wholesomememes"
-    # url = "https://meme-api.herokuapp.com/gimme" + sr 
-    url = "https://meme-api.herokuapp.com/gimme"
+    subreddit = "/wholesomememes"
+    # url = "https://meme-api.herokuapp.com/gimme" + sr
+    # This fetches memes from all over reddit, which CAN be NSFW, please make sure to uncomment the line below and comment the line below it
+    # url = "https://meme-api.com/gimme" + subreddit
+    # comment the line below to disable NSFW posts.
+    url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
